74845ed9550b254058c6a574d56d9fa92a0b9b7a
Ebony Hargro <ebonyhargro@intuitdep02569.lan>
2021-05-12 18:53:14 -0700

    Testing out step 7

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+const { Book } = require('../models');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // res.render('index', { title: 'Express' });
+ (async () => {
+     await Book.findAll();
+     console.log('TEST');
+     res.json();
+}
+ );
 });
 
 module.exports = router;
