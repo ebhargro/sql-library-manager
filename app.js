e1b40651401e3cfe5318c21402ea6c936dafb298
Ebony Hargro <ebonyhargro@intuitdep02569.lan>
2021-05-13 23:11:44 -0700

    setting up middleware

--- app.js
+++ app.js
@@ -23,20 +23,37 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+//Using a static route and the express.static method to serve te static files located in the public folder
+app.use('/static', express.static('public'));
+
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  const err = new Error();
+  console.log('Error 404');
+  err.status = 404;
+  err.message = 'Uh-oh, you are looking for a page that does not exist! Please try again.'
+  res.render('page-not-found', { err });
 });
 
-// error handler
+// 500 global error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
+  console.log('Error 500');
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+
+  if(err.status == 404) {
+    res.render('page_not_found', { title: "Page Not Found", error: err});
+  } else if (err.status === undefined) {
+    err.status = 500;
+    err.message = 'Oh no! There was an error on the server.';
+    res.render('error', { title: "Page Not Found", error: err});
+  }
 });
 //Use the sequelize.sync() method to sync the model with the database
 (async() => {
